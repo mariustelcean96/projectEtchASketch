1833ef66640c364fe0e1933ec404d9f8e7c1a8fd
Marius-Andrei Telcean <marius.telcean96@gmail.com>
2022-01-28 17:46:22 +0200

    Add a fully functional version of the project.

--- script.js
+++ script.js
@@ -2,30 +2,44 @@ const FlexContainer = document.querySelector('.flex-container');
 
 /* Constructing the container shape */
 
-function drawGrid() {
-  for (let i = 0; i < 256; i++) {
+function drawGrid(squareNum) {
+  for (let i = 1; i <= squareNum*squareNum; i++) {
     const div = document.createElement('div');
-    div.style.cssText = `width: 16px; height: 16px;`;
-    div.textContent = '.';
+    div.style.cssText = `width: ${256/squareNum}px; height: ${256/squareNum}px;`;
+    div.textContent = '';
     div.classList.toggle('flexItem');
     FlexContainer.appendChild(div);
     }
 }
-drawGrid();
+drawGrid(16);
 
-const divs = document.querySelectorAll('.flexItem');
-divs.forEach((div) => {
-div.addEventListener('mouseover', (e) => {
-  div.style.cssText += 'background-color: red';
+/* Adding the mouseover behaviour to the flex items */
+
+function addHoverBehaviour() {
+  const divs = document.querySelectorAll('.flexItem');
+  divs.forEach((div) => {
+  div.addEventListener('mouseover', (e) => {
+    div.style.cssText += 'background-color: red';
+    });
   });
-});
+}
+addHoverBehaviour();
+
+/* Resetting the grid for a fresh start */
 
 function resetGrid() {
-    const divs = document.querySelectorAll('.flexItem');
-    divs.forEach((div) => {
-      div.style.cssText = 'width: 16px; height: 16px; background-color: transparent';  
-    });
+  const divs = document.querySelectorAll('.flexItem');
+  divs.forEach((div) => {
+    div.style.cssText = 'width: 16px; height: 16px; background-color: transparent';  
+  });
+  const squareNum = prompt('How many squares should be on one side of the array? (Positive whole number)', '32');
+  const FlexContainer = document.querySelector('.flex-container');
+  FlexContainer.innerHTML = "";
+  drawGrid(squareNum);
+  addHoverBehaviour();
 }
 
 const resetButton = document.querySelector('#resetButton');
 resetButton.addEventListener('click', resetGrid);
+
+const colorButton = document.querySelector('#colorButton');
