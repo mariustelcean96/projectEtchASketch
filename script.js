db6582b99d3eeecde52db0a323f82cfe16baa7e5
Marius-Andrei Telcean <marius.telcean96@gmail.com>
2022-01-28 16:19:35 +0200

    On my way to solving this.

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const FlexContainer = document.querySelector('.flex-container');
 
-/* Contructing the container shape */
+/* Constructing the container shape */
 
-for (let i = 0; i < 10; i++) {
-  const div = document.createElement('div');
-  div.style.cssText = 'width: 32px; height: 32px; border: 0px solid white';
-  div.textContent = 'L';
-  FlexContainer.appendChild(div);
+function drawGrid() {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    div.style.cssText = `width: 16px; height: 16px;`;
+    div.textContent = '.';
+    div.classList.toggle('flexItem');
+    FlexContainer.appendChild(div);
+    }
 }
+drawGrid();
+
+const divs = document.querySelectorAll('.flexItem');
+divs.forEach((div) => {
+div.addEventListener('mouseover', (e) => {
+  div.style.cssText += 'background-color: red';
+  });
+});
+
+function resetGrid() {
+    const divs = document.querySelectorAll('.flexItem');
+    divs.forEach((div) => {
+      div.style.cssText = 'width: 16px; height: 16px; background-color: transparent';  
+    });
+}
+
+const resetButton = document.querySelector('#resetButton');
+resetButton.addEventListener('click', resetGrid);
